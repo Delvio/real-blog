3dc258eb79a65a8f58b0ffab9d092715110e2f85
Elvio Severino <elvioseverino@gmail.com>
2020-08-18 07:37:13 -0700

    Added share by email feature

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from django.views.generic import ListView
 from blog.forms import EmailPostForm
+from django.core.mail import send_mail
 
 
 class PostListView(ListView):
@@ -25,14 +26,25 @@ def post_detail(request, year, month, day, post):
 
 def post_share_email(request, post_id):
     post = get_object_or_404(Post, id=post_id, status="published")
+    sent = False
 
     if request.method == "POST":
         form = EmailPostForm(request.POST)
         if form.is_valid():
-            form.cleaned_data
-            # send email
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "{}"'.format(
+                cd["name"], cd["email"], post.title
+            )
+            message = 'read "{}" at {}\n\n{}\'s comments: {}'.format(
+                post.title, post_url, cd["name"], cd["comments"]
+            )
+            send_mail(subject, message, "myblog@elvioseverino.com", [cd["to"]])
+            sent = True
     else:
         form = EmailPostForm()
     return render(
-        request, "blog/post/share.html", {"post": post, "form": form}
+        request,
+        "blog/post/share.html",
+        {"post": post, "form": form, "sent": sent},
     )
