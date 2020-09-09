032f19a5677ecad1150d31306caebe2527f9c1c4
Delvio <elvioseverino@gmail.com>
2020-09-09 19:19:10 -0400

    fixed minor typo

--- myblog/settings/production.py
+++ myblog/settings/production.py
@@ -4,9 +4,7 @@ from myblog.settings.base import env
 
 SECRET_KEY = env("SECRET_KEY")
 
-ALLOWED_HOSTS = emv("DJANGO_ALLOWED_HOSTS")
-
-EMAIL_HOST = env("DJANGO_EMAIL_HOST")
+ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOSTS")
 
 DATABASES["default"] = env.db("DATABASE_URL")
 
